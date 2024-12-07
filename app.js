355da8fb4581e95b1a6ec3bdd666c88cc717e2a0
Ceren Tuğçe Ersan <131873406+cerenersan@users.noreply.github.com>
2024-12-07 19:39:00 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+const puppeteer = require('puppeteer');
+ 
+(async () => {
+  // 50 kez tekrarlanacak döngü
+  for (let i = 0; i < 100; i++) {
+    // Yeni tarayıcı başlatıldı
+    const browser = await puppeteer.launch({
+      headless: 'new',
+      // headless: true (default) enables old Headless;
+      // headless: 'new' enables new Headless;
+      // headless: false enables “headful” mode.
+    });
+   
+    // Yeni sayfa açıldı
+    
+    const page = await browser.newPage();
+ 
+    // Dosya yüklendi
+  
+
+
+    await page.goto("https://www.wattpad.com/1405001342-alpha-predator-%E9%A1%B6%E7%BA%A7%E6%8E%A0%E9%A3%9F%E8%80%85-bl-%E2%99%A1-76-b%C3%B6l%C3%BCm-%E2%99%A1");
+    const süre = await page.evaluate(() => {
+      return Math.floor(Math.random() * 10) + 1;
+     
+    });
+    const delayInMilliseconds = süre*50; // 1-10 arası saniye
+    await page.evaluate(delay => {
+      return new Promise(resolve => {
+        setTimeout(resolve, delay);
+      });
+    }, delayInMilliseconds);
+    console.info('Test successful.');
+    // Tarayıcıyı kapatıldı
+    await browser.close();
+  }
+})();
